b971dd18b24eb49d9a00cdf17266de1000e4c568
Ankur Jain <ankur.jain@partsavatar.ca>
2020-11-02 17:36:56 +0530

    deploy.sh added

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ pip install -r requirements.txt
 python manage.py migrate
 rm -f nohup.out
 fuser -k 8000/tcp
-nohup gunicorn -b 0.0.0.0:8000 emotorad.asgi -k gevent &
+nohup gunicorn -b 0.0.0.0:8000 emotorad.wsgi -k gevent &
