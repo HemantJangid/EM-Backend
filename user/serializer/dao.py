7baa74ee3afd1829203a4ec640a54a5fb06adceb
Ankur Jain <ankur.jain@partsavatar.ca>
2020-11-06 12:55:23 +0530

    firebase token length updated

--- user/serializer/dao.py
+++ user/serializer/dao.py
@@ -2,4 +2,4 @@ from rest_framework import serializers
 
 
 class SignupDao(serializers.Serializer):
-    firebase_token = serializers.CharField(max_length=255)
+    firebase_token = serializers.CharField()
