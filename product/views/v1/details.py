7b7270992f5a93dfce28cd1d54077045ee1480d8
Ankur Jain <ankur.jain@partsavatar.ca>
2020-11-23 16:48:53 +0530

    slug added

--- product/views/v1/details.py
+++ product/views/v1/details.py
@@ -1,3 +1,4 @@
+import uuid
 from rest_framework.views import APIView
 from core.models import ProductContent, Product
 from middleware.response import success
@@ -6,7 +7,12 @@ from product.serializer.dto import ProductContentDto
 
 class ProductDetailView(APIView):
     def get(self, request, product_id):
-        product = Product.objects.filter(uuid=product_id, is_archived=False).first()
+        query = {'is_archived': False}
+        if is_valid_uuid(product_id):
+            query["uuid"] = product_id
+        else:
+            query["slug"] = product_id
+        product = Product.objects.filter(**query).first()
         if not product:
             return success({}, "invalid product id", False)
 
@@ -15,3 +21,11 @@ class ProductDetailView(APIView):
             return success({}, "no content is present", False)
 
         return success(ProductContentDto(content).data, "details fetched successfully", True)
+
+
+def is_valid_uuid(val):
+    try:
+        uuid.UUID(str(val))
+        return True
+    except ValueError:
+        return False
