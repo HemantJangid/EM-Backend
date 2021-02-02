7a7746ef7c3b3e6836dd6f0c97277a5283ced2aa
Ankur Jain <ankur.jain@partsavatar.ca>
2021-02-02 20:36:46 +0530

    ProductAccessoryListView added

--- product/urls.py
+++ product/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views.v1.vehicle import ProductListView
+from .views.v1.vehicle import ProductVehicleListView
 from .views.v1.details import ProductDetailView
 from .views.v1.cart import CartView, GetCartView
 from .views.v1.order import OrderView, GetOrderView
@@ -8,10 +8,12 @@ from .views.v1.warranty import WarrantyView
 from .views.v1.insurance import InsuranceView
 from .views.v1.dealer import DealerView
 from .views.v1.test_ride import TestRideView
+from .views.v1.accessory import ProductAccessoryListView
 
 
 urlpatterns = [
-    url(r'^v1/product/vehicle/list$', ProductListView.as_view()),
+    url(r'^v1/product/vehicle/list$', ProductVehicleListView.as_view()),
+    url(r'^v1/product/accessory/list$', ProductAccessoryListView.as_view()),
     url(r'^v1/product/(?P<product_id>[\w\-]+)/detail$', ProductDetailView.as_view()),
     url(r'^v1/cart/(?P<product_id>[\w\-]+)$', CartView.as_view()),
     url(r'^v1/cart$', GetCartView.as_view()),
--- product/views/v1/accessory.py
+++ product/views/v1/accessory.py
@@ -1,12 +1,12 @@
 from rest_framework.views import APIView
-from core.models import Product, PRODUCT_VEHICLE
+from core.models import Product, PRODUCT_ACCESSORY
 from middleware.response import success
 from product.serializer.dto import ProductDto
 
 
-class ProductVehicleListView(APIView):
+class ProductAccessoryListView(APIView):
     def get(self, request):
-        products = Product.objects.filter(is_archived=False).filter(type=PRODUCT_VEHICLE).order_by('name').all()
+        products = Product.objects.filter(is_archived=False).filter(type=PRODUCT_ACCESSORY).order_by('name').all()
         payload = {
             'products': ProductDto(products, many=True).data
         }
