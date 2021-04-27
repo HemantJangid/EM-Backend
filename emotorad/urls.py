a8ad945a76a020626c76215b255f0e9d6de2fe12
HemantJangid <jhemant539@gmail.com>
2021-04-27 16:02:03 +0530

    added urls for static

--- emotorad/urls.py
+++ emotorad/urls.py
@@ -31,3 +31,7 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+    
+if not settings.DEBUG:
+    urlpatterns += [re_path(r'^.*',
+                            TemplateView.as_view(template_name='index.html'))]
